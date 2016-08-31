223613e551204ecf8efaf9028e36299204d9e152
Sergeijyle <James.lyle@students.aie.edu.au>
2016-08-30 17:30:25 -0700

    added header

--- Dinosaur/main.cpp
+++ Dinosaur/main.cpp
@@ -1,35 +1,4 @@
-#include <iostream>
-
-struct Dinosaur
-{
-	char Name[200];
-	char Type[200];
-	int Velocity;
-};
-
-Dinosaur createDinosaur()
-{
-	Dinosaur dino;
-	printf("Create a name for your dino, give it a type \n and tell me how fast it can run\n");
-	scanf_s("%s %s %d", dino.Name, 200, dino.Type, 200, &dino.Velocity);
-	getchar();
-	printf("%s %s %d \n", dino.Name, dino.Type, dino.Velocity);
-
-	return dino;
-}
-
-void printDinosaur(Dinosaur theDino)
-{
-	// any changes to theDino will NOT affect the original dino
-	printf("%s %s %d \n", theDino.Name, theDino.Type, theDino.Velocity);
-}
-
-void modifyDinosaur(Dinosaur &theDino)
-{
-	// any changes to theDino will affect the original dino
-	theDino.Velocity = 2;
-}
-
+#include "Header.h"
 
 void main()
 {
